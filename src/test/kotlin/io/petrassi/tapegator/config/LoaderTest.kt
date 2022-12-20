165ff0c27587877e1f9b56dbd4985f7ccce597fc
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-12-20 08:57:07 -0300

    Test: interface Loader

---
+++ src/test/kotlin/io/petrassi/tapegator/config/LoaderTest.kt
@@ -0,0 +1,48 @@
+package io.petrassi.tapegator.config
+
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class LoaderTest {
+
+    private val yamlFile = "randomDataClass.yaml"
+    private val clazz = RandomDataClass::class.java
+
+    @Test
+    fun `Load from system`() {
+        // Given
+        val loader = Loader.create()
+
+        // When
+        val dataClass = loader.load(clazz, yamlFile)
+
+        // Then
+        assertEquals(dataClass.name, "myName")
+    }
+
+    @Test
+    fun `Load from enum`() {
+        // Given
+        val loader = Loader.create(LoaderFactory.JACKSON)
+
+        // When
+        val dataClass = loader.load(clazz, yamlFile)
+
+        // Then
+        assertEquals(dataClass.name, "myName")
+    }
+
+    @Test
+    fun `Load from name`() {
+        // Given
+        val loader = Loader.create("JaCkSoN")
+
+        // When
+        val dataClass = loader.load(clazz, yamlFile)
+
+        // Then
+        assertEquals(dataClass.name, "myName")
+    }
+}
+
+data class RandomDataClass(val name: String)
