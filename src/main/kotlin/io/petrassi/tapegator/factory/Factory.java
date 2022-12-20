583fa5d7f8547b075d887a56393b0144b6508be4
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-12-20 08:50:07 -0300

    Add: interface Factory

---
+++ src/main/kotlin/io/petrassi/tapegator/factory/Factory.java
@@ -0,0 +1,23 @@
+package io.petrassi.tapegator.factory;
+
+public interface Factory<T> {
+    Class<T> getType();
+
+    default String getKeyRoot() {
+        return FactoryConfig.KEY_ROOT;
+    }
+
+    default String getKeyName(){
+        return getType().getSimpleName();
+    }
+
+    /**
+     * @return an instance of T supplied for an Enum entry which name matches the value from a system property
+     */
+    default T create() {
+        String implementationName = System.getProperty(getKeyRoot() + getKeyName(), "");
+        return create(implementationName);
+    }
+
+    T create(String implementationName);
+}
