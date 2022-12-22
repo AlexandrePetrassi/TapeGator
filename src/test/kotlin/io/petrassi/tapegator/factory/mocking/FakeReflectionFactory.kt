4f4e7db9468ac12701d55ba0a605fa1f9b99d1b7
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2022-12-21 21:30:52 -0300

    Refactor: extract factory functionality to TapeGator-Factory

--- src/test/kotlin/io/petrassi/tapegator/factory/mocking/FakeReflectionFactory.kt
+++
@@ -1,8 +0,0 @@
-package io.petrassi.tapegator.factory.mocking
-
-import io.petrassi.tapegator.factory.ReflectionFactory
-
-object FakeReflectionFactory : ReflectionFactory<FakeData> {
-    override fun defaultImplementation(): String = FakeData::class.java.simpleName
-    override fun getType(): Class<FakeData> = FakeData::class.java
-}
