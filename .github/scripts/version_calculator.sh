2aff4183854eeea0c7920f3f0d50511fb14c6af9
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-06-25 22:22:59 -0300

    BREAKING CHANGE: revise version_calculator.sh

--- .github/scripts/version_calculator.sh
+++ .github/scripts/version_calculator.sh
@@ -6,32 +6,41 @@ calculate_next_version() {
   local major
   local minor
   local patch
-  local next_version
 
+  # Extract latest tag
   latest_tag=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
+
+  # Determine commit messages since last tag
   commits=$(git log --oneline "${latest_tag}"..HEAD)
+
+  # Default version components
   major=$(cut -d. -f1 <<< "$latest_tag")
   minor=$(cut -d. -f2 <<< "$latest_tag")
   patch=$(cut -d. -f3 <<< "$latest_tag")
-  next_version=""
 
-  for commit in $commits; do
+  # Flag to track version increment
+  incremented=false
+
+  # Check commit messages and increment version components
+  while IFS= read -r commit; do
     if [[ $commit == "BREAKING CHANGE"* ]]; then
       major=$((major + 1))
-      next_version="${major}.0.0"
-      break
+      minor=0
+      patch=0
+      incremented=true
+      break  # Break out of the loop
     elif [[ $commit == "Add"* ]]; then
       minor=$((minor + 1))
       patch=0
-      next_version="${major}.${minor}.${patch}"
-      break
+      incremented=true
+      break  # Break out of the loop
     fi
-  done
+  done <<< "$commits"
 
-  if [[ $next_version == "" ]]; then
+  # Increment patch if no major or minor increment occurred
+  if ! $incremented; then
     patch=$((patch + 1))
-    next_version="${major}.${minor}.${patch}"
   fi
 
-  echo $next_version
+  echo "${major}.${minor}.${patch}"
 }
