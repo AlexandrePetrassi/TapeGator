5ad51c32ed5db3761148a4ed65ae1d518c718a91
Alexandre Petrassi Cardoso <alexandrepetrassi@gmail.com>
2023-06-25 21:37:43 -0300

    Pipeline: fix version_calculator.sh AutoIncrement

--- .github/scripts/version_calculator.sh
+++ .github/scripts/version_calculator.sh
@@ -6,25 +6,32 @@ calculate_next_version() {
   local major
   local minor
   local patch
+  local next_version
 
   latest_tag=$(git describe --tags --abbrev=0 2>/dev/null || echo "0.0.0")
   commits=$(git log --pretty=%s "$latest_tag"..HEAD)
   major=$(cut -d. -f1 <<< "$latest_tag")
   minor=$(cut -d. -f2 <<< "$latest_tag")
   patch=$(cut -d. -f3 <<< "$latest_tag")
+  next_version=""
 
   for commit in $commits; do
     if [[ $commit == "BREAKING CHANGE"* ]]; then
       major=$((major + 1))
-      minor=0
-      patch=0
+      next_version="${major}.0.0"
+      break
     elif [[ $commit == "Add"* ]]; then
       minor=$((minor + 1))
       patch=0
-    else
-      patch=$((patch + 1))
+      next_version="${major}.${minor}.0"
+      break
     fi
   done
 
-  echo "${major}.${minor}.${patch}"
+  if [[ $next_version == "" ]]; then
+    patch=$((patch + 1))
+    next_version="${major}.${minor}.${patch}"
+  fi
+
+  echo $next_version
 }
